9983ce125ed330e86411f5a12fad560483fbae2d
Nur Arisha <147675471+ar1sha55@users.noreply.github.com>
2024-06-02 20:53:44 +0800

    Update classpatient.cpp

--- classpatient.cpp
+++ classpatient.cpp
@@ -10,18 +10,11 @@ int main() {
     patient1.setID("001");
     patient1.setname("Ali bin Abu");
     patient1.setpassword("Abc123");
-    patient1.setdob("18/12/2020");
+    patient1.setdob("18/12/2004");
     patient1.setsex("M");
     patient1.setMed(&med1);
 
-    cout << "Details of Patient 1" << endl
-         << "ID: " << patient1.getID() << endl
-         << "Name: " << patient1.getname() << endl
-         << "DOB: " << patient1.getdob() << endl
-         << "Age: " << patient1.getAge() << endl
-         << "Sex: " << patient1.getsex() << endl
-         << "Medicine name: " << med1.getMedName() << endl
-         << "Medicine dosage: " << med1.getdosage() << endl;
+    patient1.printDetails();
 
     if(patient1.getAge() > 70 || patient1.getAge() < 13) 
     cout << "You need a GUARDIAN." << endl;
