dba208e6d6d449c1a9f07fce3b46feb55ee19cb5
Jasmine <147676875+tjasmiine@users.noreply.github.com>
2024-06-04 20:44:07 +0800

    Add files via upload

---
+++ Proj3/Medication.h
@@ -0,0 +1,31 @@
+//Medication class header file
+#include "MedType.h"
+#include <string>
+#ifndef MED_H
+#define MED_H
+using namespace std;
+
+class Medication {
+    string medName, dosage;
+    MedType medType;//composition
+    //Frequency frequency; //composition
+
+    public:
+        //constructor
+        Medication();
+        Medication(string , string , string , string , string );
+
+        //accessor
+        string getMedName();
+        
+        //other function
+        void input();
+        void output(int);
+        void outputMed();
+
+        //destructor
+        ~Medication();
+};
+
+
+#endif
